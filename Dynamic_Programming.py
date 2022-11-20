2656d6db36f5b322cf710b7e70783657d550cf56
2001Kaye <jhan117@naver.com>
2022-11-20 14:43:39 +0900

    [221120] 1문제

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -31,6 +31,18 @@ class Solution:
 
         return d[-1]
 
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        n = len(cost)
+        dp = [0] * n
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+
+        return min(dp[-1], dp[-2])
+
 
 if __name__ == '__main__':
     main()
