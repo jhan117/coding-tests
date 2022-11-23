86a7254da8cf72d009a75cfe8977a5b9d79bb9d5
2001Kaye <jhan117@naver.com>
2022-11-23 13:53:11 +0900

    [221123] 1문제

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -1,4 +1,16 @@
 from collections import Counter
+from unittest import TestCase, main, skip
+
+
+class Tests(TestCase):
+    @skip
+    def test_characterReplacement1(self):
+        result = Solution().characterReplacement('AABABBA', 1)
+        self.assertEqual(result, 4)
+
+    def test_characterReplacement2(self):
+        result = Solution().characterReplacement('ABAB', 2)
+        self.assertEqual(result, 4)
 
 
 class Solution:
@@ -25,3 +37,19 @@ class Solution:
             start += 1
 
         return res
+
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxlen, largestCount = 0, 0
+        arr = Counter()
+        for idx in range(len(s)):
+            arr[s[idx]] += 1
+            largestCount = max(largestCount, arr[s[idx]])
+            if maxlen - largestCount >= k:
+                arr[s[idx - maxlen]] -= 1
+            else:
+                maxlen += 1
+        return maxlen
+
+
+if __name__ == '__main__':
+    main()
