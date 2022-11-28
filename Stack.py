7ea695d2462f7e5e7b1c13c07d574de2ca839a9d
2001Kaye <jhan117@naver.com>
2022-11-28 09:52:21 +0900

    [221128] 1문제

--- Stack.py
+++ Stack.py
@@ -16,3 +16,25 @@ class Solution:
                 t_text.pop()
 
         return s_text == t_text
+
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curNum = 0
+        curString = ''
+
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+            print(stack, curNum, curString)
+        return curString
