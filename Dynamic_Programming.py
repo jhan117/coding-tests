5a9e68f8221e643d394dee3093d140b03771d0a2
2001Kaye <jhan117@naver.com>
2022-12-01 08:27:30 +0900

    [221201] 1문제

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -32,16 +32,10 @@ class Solution:
         return d[-1]
 
     def minCostClimbingStairs(self, cost: list[int]) -> int:
-        n = len(cost)
-        dp = [0] * n
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
 
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-
-        for i in range(2, n):
-            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
-
-        return min(dp[-1], dp[-2])
+        return min(cost[-2], cost[-1])
 
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0] * n for _ in range(m)]
