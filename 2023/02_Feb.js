91d3af866985968a530fd54b7ffe86424baa9da6
2001Kaye <jhan117@naver.com>
2023-02-03 17:57:50 +0900

    close #19; [Baekjoon] 230203 | Silver 2 | 1260

--- 2023/02_Feb.js
+++ 2023/02_Feb.js
@@ -20,3 +20,32 @@ function makeOne(N) {
 }
 
 console.log(makeOne(Number(N)));
+
+// 인접 행렬
+const graph = Array.from({ length: n + 1 }, () => []);
+/**
+ * [23.02.03 - Baekjoon] 1260 DFS와 BFS
+ * @param {boolean} isBFS true이면 BFS, false이면 DFS
+ * @returns {string} 방문한 순서
+ */
+function DFSandBFS(isBFS = false) {
+  if (isBFS) graph.map((nodes) => nodes.sort((a, b) => a - b));
+  else graph.map((nodes) => nodes.sort((a, b) => b - a));
+  const visited = Array(n + 1).fill(false);
+  const data = [v];
+  const answer = [];
+
+  while (data.length) {
+    if (isBFS) var node = data.shift();
+    else var node = data.pop();
+    if (!visited[node]) {
+      visited[node] = true;
+      answer.push(node);
+      data.push(...graph[node]);
+    }
+  }
+  return answer.join(" ");
+}
+
+console.log(DFSandBFS());
+console.log(DFSandBFS(true));
