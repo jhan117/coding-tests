a1b2a04dcc7bd2df0ede0ac6f67f6faf160dfe4f
2001Kaye <jhan117@naver.com>
2023-11-29 17:38:43 +0900

    [231129] Java 3

--- Java/Sprout/input.java
+++ Java/Sprout/input.java
@@ -11,12 +11,50 @@ public class input {
         sNum = sc.nextInt();
 
         System.out.println(fNum+sNum);
-        
-        sc.close();
+    }
+
+    static void _1001(){
+        // 1001.A-B
+        Scanner sc = new Scanner(System.in);
+
+        int fNum, sNum;
+        fNum = sc.nextInt();
+        sNum = sc.nextInt();
+
+        System.out.println(fNum-sNum);
+    }
+
+    static void _10998(){
+        // 10998.AxB
+        Scanner sc = new Scanner(System.in);
+
+        int fNum, sNum;
+        fNum = sc.nextInt();
+        sNum = sc.nextInt();
+
+        System.out.println(fNum*sNum);
+    }
+
+    static void _10869(){
+        // 10869.사칙연산
+        Scanner sc = new Scanner(System.in);
+
+        int fNum, sNum;
+        fNum = sc.nextInt();
+        sNum = sc.nextInt();
+
+        System.out.println(fNum+sNum);
+        System.out.println(fNum-sNum);
+        System.out.println(fNum*sNum);
+        System.out.println(fNum/sNum);
+        System.out.println(fNum%sNum);
     }
 
     public static void main(String[] args) {
         input._1000();
+        input._1001();
+        input._10998();
+        input._10869();
     }
     
 }
