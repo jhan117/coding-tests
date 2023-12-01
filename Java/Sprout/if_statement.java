49cd7796f6cb696e3feaed7e57ccd77cbb517a54
2001Kaye <jhan117@naver.com>
2023-12-01 16:21:11 +0900

    [231201] Java 3

--- Java/Sprout/if_statement.java
+++ Java/Sprout/if_statement.java
@@ -18,7 +18,57 @@ public class if_statement {
         }
     }
 
+    static void _9498(){
+        // 9498.시험 성적
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (num >= 90) {
+            System.out.println("A");
+        } else if (num >= 80){
+            System.out.println("B");
+        } else if (num >= 70) {
+            System.out.println("C");
+        } else if (num >= 60){
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+
+    static void _14681(){
+        // 14681.사분면 고르기
+        Scanner sc = new Scanner(System.in);
+        
+        int x, y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        
+        if (y > 0) {
+            System.out.println((x > 0) ?  1 : 2);
+        } else if (y < 0) {
+            System.out.println((x > 0) ?  4 : 3);
+        }
+    }
+
+    static void _2753(){
+        // 2753.윤년
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
     public static void main(String[] args) {
         if_statement._1330();
+        if_statement._9498();
+        if_statement._14681();
+        if_statement._2753();
     }
 }
